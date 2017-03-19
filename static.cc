0245bf10e2f5004177a308586140ff1c8bf54482
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2017-03-18 20:17:50 -0700

    Parse formatted lines from trace file
    Assumes lines are formatted as follows:
    program_counter  branch_type  target_address  taken_bit
    (e.g "1a204 2 17fe8 1")

--- static.cc
+++ static.cc
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <sstream>
 #include <unistd.h> // getopt()
 
 void usage(char *baseName);
+void simulate(std::string filePath, bool verbose);
 
 int main(int argc, char * argv[]) {
     std::string traceFilePath;
@@ -28,10 +31,38 @@ int main(int argc, char * argv[]) {
     
     traceFilePath = argv[optind];
     
+    simulate(traceFilePath, vflag);
     
     return 0;
 }
 
 void usage(char *baseName) {
     std::cerr << "Usage: " << baseName << " [-v] TRACE_FILE" << std::endl;
+}
+
+void simulate(std::string filePath, bool verbose) {
+    std::ifstream traceFile;
+    std::string line;
+    unsigned int programCounter, targetAddress;
+    unsigned short branchType;
+    bool branchTaken;
+    
+    traceFile.open(filePath);
+    if (traceFile.fail()) {
+        std::cerr << "Error: failed to open: " << filePath << std::endl;
+        exit(1);
+    }
+    
+    while(std::getline(traceFile, line)) {
+        std::stringstream sstream(line);
+        sstream >> std::hex >> programCounter;
+        sstream >> std::dec >> branchType;
+        sstream >> std::hex >> targetAddress;
+        sstream >> std::dec >> branchTaken;
+        
+        std::cout << programCounter << " " << branchType << " ";
+        std::cout << targetAddress << " " << branchTaken << std::endl;
+    }
+    
+    traceFile.close();
 }
